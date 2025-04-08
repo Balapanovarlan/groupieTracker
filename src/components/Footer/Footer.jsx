4604ab443ef2f7f37822e92083a54b296e2c0c91
arlan balapanov <mrx11y@gmail.com>
2025-04-08 22:21:16 +0500

    Footer Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography, Link as MuiLink } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { PageRoutes } from '../../routes/PageRoutes'; 
+import tgIcon from '../../assets/icons/tg.svg'
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-} 
+    <Box
+      component="footer"
+      sx={{
+        mt: 'auto',
+        py: 3,
+        px: 2,
+        backgroundColor: '#f5f5f5',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        fontSize:'1.5rem',
+      }}
+    >
+      <Typography variant="body2" color="text.secondary">
+        © {new Date().getFullYear()} MyGroupieTracker
+      </Typography>
 
-export default Footer
+      <Box sx={{ display: 'flex', gap: 2 }}>
+        <MuiLink component={Link} to={PageRoutes.CommonRoutes.home} underline="hover">
+          Home
+        </MuiLink>
+        <MuiLink component={Link} to={PageRoutes.FavoritesRoutes.favorites} underline="hover">
+          Favorites
+        </MuiLink>
+      </Box>
+
+      <Box sx = {{display: 'flex', alignItems: 'center', gap:2}}>
+      <Typography variant="h6" color="text.secondary">
+      Contact: @Arlandia
+      </Typography>
+      <img src={tgIcon} alt="" width={30} height={30}/>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
